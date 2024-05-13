c6f34c759031c57b0ce2a05e751fb414ec74dd0f
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-05-13 16:37:28 +0700

    mbrstatik 1

---
+++ mbrstatik.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa
+{
+public:
+    static int nim;
+    int Id;
+    string nama;
+
+    void setId();
+
+    void printAll();
+    Mahasiswa(string pNama) : nama(pNama)
+    {
+        setId();
+    }
+    
+
+    
+      
+};
+
+int Mahasiswa::nim = 20;
+void Mahasiswa::setId()
+{
+    Id = nim++;
+}
+
+void Mahasiswa::printAll()
+{
+    cout << " ID = " << Id << endl;
+    cout << " Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main()
+{
+    Mahasiswa mhs1("Lia Kurnia");
+    Mahasiswa mhs2("Asroni");
+    Mahasiswa mhs3("Andi Kurniwan");
+    Mahasiswa mhs4("Joko Purbo");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+    return 0;
+}
